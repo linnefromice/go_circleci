496f1337faa34fda44b25ef8aa31f0a8d19dbc0e
linnefromice <linnefromice@gmail.com>
2020-02-22 13:34:07 +0900

    add test (duplicate logic)

--- sample01_test.go
+++ sample01_test.go
@@ -13,6 +13,14 @@ func Test_HelloWorld_01(t *testing.T) {
 }
 
 func Test_HelloWorld_02(t *testing.T) {
+	actual := HelloWorld("12345")
+	expected := "Hello World, 12345!"
+	if actual != expected {
+		t.Errorf("actual %v\nwant %v", actual, expected)
+	}
+}
+
+func Test_HelloWorld_03(t *testing.T) {
 	actual := HelloWorld("")
 	expected := "Hello World!"
 	if actual != expected {
